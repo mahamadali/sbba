50484667771f66cc45a171d0f2749a641cf7c49c
Mahamad Ali <manknojiya121@gmail.com>
2022-08-10 17:39:07 +0530

    general

--- segments/views/app.jly.php
+++ segments/views/app.jly.php
@@ -18,6 +18,14 @@
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+
+      <style type="text/css">
+          body {
+              font-family: 'Poppins', sans-serif;
+          }
+      </style>
+
 
         @plot('styles')
     </head>
