a032f7fa489a3c5732398c935b486b3505cf59a8
Rehan <rehanmasiya786@gmail.com>
2022-09-08 12:57:35 +0530

    Change photos in sbba -Rehan

--- segments/views/welcome.jly.php
+++ segments/views/welcome.jly.php
@@ -57,10 +57,12 @@
   <div data-v-70ba3ad3="" class="section1__inner">
      <div data-v-70ba3ad3="" class="section1__inner-txt">
         <h1 data-v-70ba3ad3="">How To Join</h1>
-        <h2 class="home-page-text-color section-3">Sign-up today — we’ll get you out of the office (or off the laptop) and have a beverage in your hand in no time. Let's have fun! And make getting referrals easier for all of us.</h2>
+        <h2 class="home-subtitle home-page-text-color">Sign-up today — we’ll get you out of the office (or off the laptop) and have a beverage in your hand in no time. Let's have fun! And make getting referrals easier for all of us.</h2>
         <a href="{{ route('auth.sign-up')}}" class="ui-btn home-signup-footer">Sign Up</a>
     </div>
-    <div data-v-70ba3ad3="" class="section1__inner-img" style="margin-bottom:7.5rem;margin-right:3.5rem"><img data-v-70ba3ad3="" src="{{ url('assets/frontend/img/slide3-img.png') }}"></div>
+    <div data-v-70ba3ad3="" class="section1__inner-img">
+        
+    </div>
 </div>
 </section>
 
