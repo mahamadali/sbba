a44ea3f7905fee7d068b888a9b3865b5d33e461e
Mahamad Ali <manknojiya121@gmail.com>
2022-10-08 13:42:45 +0530

    thank you page after signup

--- segments/views/auth/signup-thank-you.jly.php
+++ segments/views/auth/signup-thank-you.jly.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <title>Thank you for signup - {{ setting('app.title') }}</title>
   <link rel="stylesheet" type="text/css" href="{{ url('assets/frontend/css/signup.css') }}">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
